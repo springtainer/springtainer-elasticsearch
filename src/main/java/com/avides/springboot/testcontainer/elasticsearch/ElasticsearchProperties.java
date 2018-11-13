c802f1226898542aabbc582c661260863bc268c4
Julian Eggers <eggers.julian@gmail.com>
2018-11-13 18:30:07 +0100

    JDK11 & SpringBoot 2.1 & Build improved

--- src/main/java/com/avides/springboot/testcontainer/elasticsearch/ElasticsearchProperties.java
+++ src/main/java/com/avides/springboot/testcontainer/elasticsearch/ElasticsearchProperties.java
@@ -22,6 +22,6 @@ public class ElasticsearchProperties extends AbstractEmbeddedContainerProperties
 
     public ElasticsearchProperties()
     {
-        setDockerImage("docker.elastic.co/elasticsearch/elasticsearch:5.6.10");
+        setDockerImage("docker.elastic.co/elasticsearch/elasticsearch:5.6.12");
     }
 }
